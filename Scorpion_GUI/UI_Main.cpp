319f9da11c1abd9e5009a49c1afe36f26f540f64
Jeroen P. Broks <codeberg@gmx.com>
2026-03-03 23:12:29 +0100

    Main UI expansion

--- Scorpion_GUI/UI_Main.cpp
+++ Scorpion_GUI/UI_Main.cpp
@@ -1,33 +1,34 @@
 // License:
-// 
+//
 // Scorpion
 // Main User Interface Manager
-// 
-// 
-// 
+//
+//
+//
 // 	(c) Jeroen P. Broks, 2026
-// 
+//
 // 		This program is free software: you can redistribute it and/or modify
 // 		it under the terms of the GNU General Public License as published by
 // 		the Free Software Foundation, either version 3 of the License, or
 // 		(at your option) any later version.
-// 
+//
 // 		This program is distributed in the hope that it will be useful,
 // 		but WITHOUT ANY WARRANTY; without even the implied warranty of
 // 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // 		GNU General Public License for more details.
 // 		You should have received a copy of the GNU General Public License
 // 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // 	Please note that some references to data like pictures or audio, do not automatically
 // 	fall under this licenses. Mostly this is noted in the respective files.
-// 
+//
 // Version: 26.02.28 I
 // End License
 #include <SlyvString.hpp>
 #include <SlyvQCol.hpp>
 
 #include "UI_Main.hpp"
+#include "UI_Overview.hpp"
 
 using namespace Slyvina::TQSG;
 using namespace Slyvina::TQSE;
@@ -48,19 +49,31 @@ namespace Slyvina {
 			if (!Register.count(Key)) {
 				Register[Key].Tag=Key;
 				Register[Key].Gadget=CreatePanel(0,0,WorkScreen()->W(),WorkScreen()->H(),WorkScreen());
+				Register[Key].Gadget->Visible=true;
 				QCol->Doing("Created Panel",Key);
 			}
 			return Register[Key].Gadget;
 		}
 
+		void FatalError(std::string M) {
+			QCol->Error(M);
+			TQSE::Notify("Fatal Error:\n\n"+M);
+			GoOn=false;
+		}
+
 
 		void Init(){
 			Graphics(1800,1200,"Scorpion");
+			InitOverview();
+			CB::Activate("OVERVIEW");
 		}
 		void Run(){
 			while(GoOn){
+				Cls();
 				Poll();
 				if (AppTerminate()) {GoOn=false; return;}
+				WorkScreen()->Draw();
+				Flip();
 			}
 		}
 
--- Scorpion_GUI/UI_Main.hpp
+++ Scorpion_GUI/UI_Main.hpp
@@ -1,27 +1,27 @@
 // License:
-// 
+//
 // Scorpion
 // Main User Interface Manager (header)
-// 
-// 
-// 
+//
+//
+//
 // 	(c) Jeroen P. Broks, 2026
-// 
+//
 // 		This program is free software: you can redistribute it and/or modify
 // 		it under the terms of the GNU General Public License as published by
 // 		the Free Software Foundation, either version 3 of the License, or
 // 		(at your option) any later version.
-// 
+//
 // 		This program is distributed in the hope that it will be useful,
 // 		but WITHOUT ANY WARRANTY; without even the implied warranty of
 // 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // 		GNU General Public License for more details.
 // 		You should have received a copy of the GNU General Public License
 // 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // 	Please note that some references to data like pictures or audio, do not automatically
 // 	fall under this licenses. Mostly this is noted in the respective files.
-// 
+//
 // Version: 26.02.28
 // End License
 #include <map>
@@ -38,8 +38,10 @@ namespace Slyvina{
 		public:
 			CB(){};
 			static void Activate(std::string key);
-			June19::j19gadget* Get(std::string key);
+			static June19::j19gadget* Get(std::string key);
 		};
+
+		void FatalError(std::string);
 
 		void Init();
 		void Run();
