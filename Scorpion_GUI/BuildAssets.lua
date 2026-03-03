3db0f2da37284f49c699127b74d8fd89611ba99d
Jeroen P. Broks <codeberg@gmx.com>
2026-03-03 23:13:04 +0100

    Jalondi Script

---
+++ Scorpion_GUI/BuildAssets.lua
@@ -0,0 +1,9 @@
+-- Jalondi Pack Script for Scorpion
+
+print("Jalondi is gonna build all this, eh?")
+Jalondi.Add("../../../../../Fonts/DosFont/Unpacked","Fonts/System.jfbf","zlib","","","System Font")
+Jalondi.Add("../../../../../Fonts/jfbf/Ryanna","Fonts/Ryanna.jfbf","zlib","","","Ryanna Font")
+Jalondi.Add("../../../../../Fonts/jfbf/Mini","Fonts/Mini.jfbf","zlib","","","Mini Font")
+Jalondi.Add("Assets","","zlib")
+Jalondi.Add("../Icon/Basis.png","GFX/Icon.png","zlib","Public Domain")
+Jalondi.Start("../Exe/Scorpion.jcr")
